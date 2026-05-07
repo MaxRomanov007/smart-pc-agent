155165f3ea2b7a21c47329bcf42190d7b9d869a4
Max Romanov <maksipapa999@gmail.com>
2026-05-07 09:00:50 +0300

    feat: extend Lua message dialogs with warn/error variants (master)
    - Add warn and error dialog helpers to Lua messages module
    - Register warn/error functions and document their parameters
    - Remove window icon usage from info dialog to simplify API

--- internal/mqtt/commands/lua-api/messages/messages.go
+++ internal/mqtt/commands/lua-api/messages/messages.go
@@ -1,7 +1,6 @@
 package messages
 
 import (
-	"smart-pc-agent/data/assets"
 	luaApi "smart-pc-agent/internal/lib/lua-api"
 
 	"github.com/ncruces/zenity"
@@ -15,11 +14,15 @@ func New() *Module {
 }
 
 const (
-	infoMessage = "info"
+	infoMessage  = "info"
+	warnMessage  = "warn"
+	errorMessage = "error"
 )
 
 func (m *Module) Register(l *lua.LState, table *lua.LTable) {
 	l.SetField(table, infoMessage, m.infoDialog(l))
+	l.SetField(table, warnMessage, m.warnDialog(l))
+	l.SetField(table, errorMessage, m.ErrorDialog(l))
 }
 
 func (m *Module) infoDialog(l *lua.LState) lua.LValue {
@@ -27,8 +30,31 @@ func (m *Module) infoDialog(l *lua.LState) lua.LValue {
 		_ = zenity.Info(
 			l.Get(-1).String(),
 			zenity.Title(l.Get(-2).String()),
-			zenity.WindowIcon(assets.GetIcon()),
 			zenity.InfoIcon,
+		)
+
+		return 0
+	})
+}
+
+func (m *Module) warnDialog(l *lua.LState) lua.LValue {
+	return l.NewFunction(func(l *lua.LState) int {
+		_ = zenity.Warning(
+			l.Get(-1).String(),
+			zenity.Title(l.Get(-2).String()),
+			zenity.WarningIcon,
+		)
+
+		return 0
+	})
+}
+
+func (m *Module) ErrorDialog(l *lua.LState) lua.LValue {
+	return l.NewFunction(func(l *lua.LState) int {
+		_ = zenity.Error(
+			l.Get(-1).String(),
+			zenity.Title(l.Get(-2).String()),
+			zenity.ErrorIcon,
 		)
 
 		return 0
@@ -41,6 +67,36 @@ func (m *Module) Doc() luaApi.ModuleDoc {
 		Functions: map[string]luaApi.FunctionDoc{
 			infoMessage: {
 				Description: "info message dialog",
+				Params: []luaApi.ParamDoc{
+					{
+						Name:        "title",
+						Type:        luaApi.TypeString,
+						Description: "dialog title",
+					},
+					{
+						Name:        "text",
+						Type:        luaApi.TypeString,
+						Description: "dialog message text",
+					},
+				},
+			},
+			warnMessage: {
+				Description: "warn message dialog",
+				Params: []luaApi.ParamDoc{
+					{
+						Name:        "title",
+						Type:        luaApi.TypeString,
+						Description: "dialog title",
+					},
+					{
+						Name:        "text",
+						Type:        luaApi.TypeString,
+						Description: "dialog message text",
+					},
+				},
+			},
+			errorMessage: {
+				Description: "error message dialog",
 				Params: []luaApi.ParamDoc{
 					{
 						Name:        "title",
